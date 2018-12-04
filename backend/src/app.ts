cbc163273ed2a474499aae980adee1c661262e7b
Denis Hristov <denishristoov@gmail.com>
2018-12-04 17:31:30 +0200

    Adding socket.io to the mix
    Also updated debug configuration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import bluebird from 'bluebird'
 import bodyParser from 'body-parser'
 import compression from 'compression'
+import errorHandler from 'errorhandler'
 import express from 'express'
 import lusca from 'lusca'
 import mongoose from 'mongoose'
 import path from 'path'
+import socketio from 'socket.io'
 
 import { MONGODB_URI } from './util/env'
 
@@ -39,4 +41,32 @@ app.get('/', (_, res) => {
 	res.sendStatus(204)
 })
 
-export default app
+
+/**
+ * Error Handler. Provides full stack - remove for production
+ */
+process.env.NODE_ENV !== 'production' && app.use(errorHandler())
+
+const server = require('http').createServer(app)
+
+/**
+ * Start Express server.
+ */
+server.listen(app.get('port'), () => {
+	console.log(
+		'App is running at http://localhost:%d in %s mode',
+		app.get('port'),
+		app.get('env')
+	)
+})
+
+const io = socketio(server)
+io.on('connection', function(client) {
+	console.log('connection', arguments)
+	client.on('event', function() {
+		console.log('event', arguments)
+	})
+	client.on('disconnect', function() {
+		console.log('disconnect', arguments)
+	})
+})
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import io from 'socket.io-client'
+
 import App from './App';
+
+import './index.css';
+
+
+const socket = io('http://localhost:4000');
+socket.on('connect', function() {
+	console.log('connect', arguments)
+});
+socket.on('event', function() {
+	console.log('event', arguments)
+});
+socket.on('disconnect', function() {
+	console.log('disconnect', arguments)
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
