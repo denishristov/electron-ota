e58d196a8a7fb4d0e195ad5ccdae1128dd16c402
Denis Hristov <denishristoov@gmail.com>
2019-02-27 15:40:38 +0200

    Checking for required environment variables on startup

--- packages/backend/src/config/index.ts
+++ packages/backend/src/config/index.ts
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv'
 import fs from 'fs'
+import invariant from 'invariant'
+
 import logger from '../util/logger'
 
 if (fs.existsSync('.env')) {
@@ -19,20 +21,28 @@ export const MONGODB_URI = isProduction ? process.env.MONGODB_URI : process.env.
 export const PORT = process.env.PORT || 4000
 
 export const PASS_SECRET_KEY = process.env.PASS_SECRET_KEY
+invariant(PASS_SECRET_KEY, 'PASS_SECRET_KEY is undefined')
 
 export const REGISTER_KEY = process.env.REGISTER_KEY
+invariant(REGISTER_KEY, 'REGISTER_KEY is undefined')
 
 export const AWS_CREDENTIALS = {
 	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
 	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 	region: process.env.AWS_REGION,
 }
+invariant(AWS_CREDENTIALS.accessKeyId, 'AWS_ACCESS_KEY_ID is undefined')
+invariant(AWS_CREDENTIALS.secretAccessKey, 'AWS_SECRET_ACCESS_KEY is undefined')
+invariant(AWS_CREDENTIALS.region, 'AWS_REGION is undefined')
 
 export const S3_CONFIG = {
 	ACL: process.env.S3_ACL,
 	Bucket: process.env.S3_BUCKET,
 	Expires: parseInt(process.env.S3_EXPIRES, 10),
 }
+invariant(S3_CONFIG.ACL, 'S3_ACL is undefined')
+invariant(S3_CONFIG.Bucket, 'S3_BUCKET is undefined')
+invariant(S3_CONFIG.Expires, 'S3_EXPIRES is undefined or is NaN')
 
 if (!MONGODB_URI) {
 	logger.error('No mongo connection string. Set MONGODB_URI environment variable.')
